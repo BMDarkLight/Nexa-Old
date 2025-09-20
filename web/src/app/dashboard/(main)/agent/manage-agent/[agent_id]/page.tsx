cc4514f48734bbd9520cf4dd5901adc616882fa7
mostafaNakhli <smostafanakhli5@gmail.com>
2025-09-20 16:27:09 +0330

    fix Linter problem

--- web/src/app/dashboard/(main)/agent/manage-agent/[agent_id]/page.tsx
+++ web/src/app/dashboard/(main)/agent/manage-agent/[agent_id]/page.tsx
@@ -4,14 +4,15 @@
 import React from "react";
 import EditAgent from "../../components/EditAgent";
 
+interface PageParams {
+  agent_id: string;
+}
+
 interface ManageAgentPageProps {
-  params: {
-    agent_id: string;
-  };
+  params: PageParams;
 }
 
 export default function ManageAgentPage({ params }: ManageAgentPageProps) {
   const { agent_id } = params;
-
   return <EditAgent agentId={agent_id} />;
 }
