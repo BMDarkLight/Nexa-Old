efdb72d6ba6d20aead28d84ba3f618fca997a800
mostafaNakhli <smostafanakhli5@gmail.com>
2025-09-20 16:40:09 +0330

    type of params

--- web/src/app/dashboard/(main)/agent/manage-agent/[agent_id]/page.tsx
+++ web/src/app/dashboard/(main)/agent/manage-agent/[agent_id]/page.tsx
@@ -1,18 +1,16 @@
-// src/app/dashboard/(main)/agent/manage-agent/[agent_id]/page.tsx
-"use client";
-
 import React from "react";
 import EditAgent from "../../components/EditAgent";
 
-interface PageParams {
-  agent_id: string;
-}
-
 interface ManageAgentPageProps {
-  params: PageParams;
+  params: {
+    agent_id: string;
+  };
 }
 
-export default function ManageAgentPage({ params }: ManageAgentPageProps) {
-  const { agent_id } = params;
+export default async function ManageAgentPage({
+  params,
+}: ManageAgentPageProps) {
+  const { agent_id } = await Promise.resolve(params);
+
   return <EditAgent agentId={agent_id} />;
 }
